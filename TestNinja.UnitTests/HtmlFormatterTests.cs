2500f40d28127dd87d841316bb492f84b474754b
Ali Moradi <moradi1101@outlook.com>
2023-12-14 18:00:52 +0330

    HtmlFormatter + IgnoreCase

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _htmlFormatter = new HtmlFormatter();
+        }
+
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
+        {
+            var result = _htmlFormatter.FormatAsBold("abc");
+
+            //Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase); // Ignore Case Sensetive
+
+            //General
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+
+            //Less General
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
